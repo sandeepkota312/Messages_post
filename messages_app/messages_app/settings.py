33ce78c0aee65dae31805dbaa68cc7973a12b3df
Sandeep Kota <59409463+sandeepkota312@users.noreply.github.com>
2023-01-23 18:15:53 +0530

    set debug=True

--- messages_app/messages_app/settings.py
+++ messages_app/messages_app/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-de)czbsfzn!^=cpc3)hm@i8-^cz!6o_znauf+w+l2@=ky_ad06'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
